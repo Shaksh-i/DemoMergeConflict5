b2a110e702bd1a3219cda26b9adf3a77d4ae618e
Shakshi-i <shakshisingh241@gmail.com>
2025-09-10 16:42:39 +0530

    member 2 added multiply

--- calculator.py
+++ calculator.py
@@ -5,3 +5,10 @@ class Calculator:
  
    def add(self):
        return self.a + self.b
+class Calculator:
+   def __init__(self, a, b):
+       self.a = a
+       self.b = b
+ 
+   def multiply(self):
+       return self.a * self.b
